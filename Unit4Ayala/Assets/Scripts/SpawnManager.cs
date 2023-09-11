02f3d52fbd47789b70da3d91683b82682d307fba
715018 <715018@student.dusd.net>
2023-09-11 11:40:16 -0700

    did stuff

--- Unit4Ayala/Assets/Scripts/SpawnManager.cs
+++ Unit4Ayala/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,13 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9;
     public int enemyCount;
     public int waveNumber = 1;
+    public GameObject powerupPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
@@ -39,6 +41,8 @@ public class SpawnManager : MonoBehaviour
         if (enemyCount ==0)
         {
             waveNumber++;  SpawnEnemyWave(waveNumber);
+
+            Instantiate(powerupPrefab,GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
     }
 }
